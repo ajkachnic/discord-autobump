c47f8a1ab1ebc350c89334ea82b31b9bed778ec4
Andrew Kachnic <ajkachnic@protonmail.com>
2020-02-17 15:12:36 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+auto-bump
+
+## Setup
+Run `npm install`
+Create a `.env` file and include the following variables
+```
+TOKEN="your discord user authentication token"
+BUMP_URL="the url of the channel you want the bot to bump in"
+```
+
+## Note
+If you are self-hosting the bot or putting it on a VPS, you can remove `keep_alive.js` and the first line of `index.js`
---
+++ index.js
@@ -0,0 +1,35 @@
+const keepAlive = require('./keep_alive')
+var cron = require('node-cron');
+const dotenv = require('dotenv').config()
+
+
+function bump() {
+  req.send({
+    "content": "!d bump",
+    "tts": false
+  });
+  
+  req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+  
+    console.log(res.body);
+  });
+}
+
+
+
+var unirest = require("unirest");
+
+var req = unirest("POST", process.env.BUMP_URL);
+
+req.headers({
+  "Authorization": process.env.TOKEN,
+  "Content-Type": "application/json"
+});
+
+req.type("json");
+cron.schedule('0 */2 * * *', () => {
+  bump()
+})
+
+bump()
