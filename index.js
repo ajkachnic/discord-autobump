052e6fc80b67dbb03f004b5b72f90c026ebdd7e7
Andrew <ajkachnic@protonmail.com>
2020-05-13 20:04:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-require('./keep_alive')
-var cron = require('node-cron');
-const dotenv = require('dotenv').config()
-
-
-function bump() {
-  req.send({
-    "content": "!d bump",
-    "tts": false
-  });
-  
-  req.end(function (res) {
-    if (res.error) throw new Error(res.error);
-  
-    console.log(res.body);
-  });
-}
-
-
-
+require("./keep_alive");
+var cron = require("node-cron");
+require("dotenv").config();
 var unirest = require("unirest");
 
 var req = unirest("POST", process.env.BUMP_URL);
 
 req.headers({
-  "Authorization": process.env.TOKEN,
-  "Content-Type": "application/json"
+  Authorization: process.env.TOKEN,
+  "Content-Type": "application/json",
 });
 
 req.type("json");
-cron.schedule('0 */2 * * *', () => {
-  bump()
-})
+function bump() {
+  req.send({
+    content: "!d bump",
+  });
 
-bump()
+  req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+    console.log(res.body);
+  });
+}
+cron.schedule("0 */2 * * *", () => {
+  bump();
+});
